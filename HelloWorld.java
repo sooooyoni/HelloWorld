7aedf81cc26433c91a9f204236798e93bed862c7
unknown <ohno3336@naver.com>
2021-05-04 13:17:43 +0900

    2021050 4
    4
    <>
    git status
    
     git commit -m 2021050

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package java_20210429;
+//폰트바꾸는방법
+//window>Preference>font입력>java text editor font 
+public class HelloWorld {
+	  public static void main(String[] args) {
+		   System.out.println("Hello World");
+	  }
+} 
